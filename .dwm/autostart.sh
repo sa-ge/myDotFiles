0840eff6bc1705941c056e6d7d7e0b5ec85a2511
alhelaly <alhe22lali@gmail.com>
2020-10-04 09:34:56 +0300

    -

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -4,8 +4,15 @@
 # execute all .sh files in /sage/.local/bin/*.sh
 
 
+exec xautolock -time 1 -locker slock&
 if [ -d /home/sage/.local/bin ] ; then
  for f in /home/sage/.local/bin/?*.sh ; do
+ [ -x "$f" ] && . "$f"
+ done
+ unset f
+fi
+if [ -d /home/sage/.local/bin2 ] ; then
+ for f in /home/sage/.local/bin2/?*.sh ; do
  [ -x "$f" ] && . "$f"
  done
  unset f
@@ -14,4 +21,5 @@ fi
 exec compton&
 sleep 1s && $HOME/.screenlayout/.MultiMonitor.sh&
 sleep 1s && exec nitrogen --restore
+cd
 #st -e  echo "switch to another Terminal Emulator sleep 10s"  sleep 10s 
