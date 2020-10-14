8b5e9268abc13d6e04a7c4160c24d4e0e86b2774
alhelaly <alhe22lali@gmail.com>
2020-10-15 00:30:50 +0300

    -

--- .local/bin/keyBinds.sh
+++ .local/bin/keyBinds.sh
@@ -1,11 +1,9 @@
 #!/bin/sh 
 
+#xmodmap $HOME/.Xmodmap
 xbindkeys
-
 # allow changing keyboard layout between English us and Arabic 
-
-setxkbmap -layout us,ara -option grp:alt_caps_toggle
-
+setxkbmap -layout us,ara -option grp:menu_toggle && xmodmap $HOME/.Xmodmap
 # change function between caps and escape - need it for vim.
 #setxkbmap -option caps:escape 
 
