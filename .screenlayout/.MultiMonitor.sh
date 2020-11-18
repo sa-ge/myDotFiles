c867f0d8ff25253c7ade71bf6c800d6eed8aee2a
alhelaly <alhe22lali@gmail.com>
2020-11-19 02:29:30 +0300

    -

--- .screenlayout/.MultiMonitor.sh
+++ .screenlayout/.MultiMonitor.sh
@@ -1,6 +1,2 @@
 #!/bin/sh
-if  xrandr | grep "HDMI1 connected" ; then
-    xrandr --output eDP1 --primary --mode 1366x768 --pos 0x0 --rotate normal --output HDMI1 --mode 1280x1024 --pos 1366x0 --rotate normal --output VIRTUAL1 --off
-else 
-    echo "not found"
-fi
+xrandr --output eDP1 --primary --mode 1366x768 --pos 0x0 --rotate normal --output HDMI1 --mode 1920x1080 --pos 1555x0 --rotate normal --output VIRTUAL1 --off
