b23a42ff917a263935507156d0d565f38401c03c
alhelaly <alhe22lali@gmail.com>
2021-01-22 04:18:27 +0300

    -

--- .local/bin/keyBinds.sh
+++ .local/bin/keyBinds.sh
@@ -1,12 +1,12 @@
 #!/bin/sh 
 
 #xmodmap $HOME/.Xmodmap
-
 # allow changing keyboard layout between English us and Arabic 
 setxkbmap -layout us,ara -option grp:menu_toggle && xmodmap $HOME/.Xmodmap && xbindkeys
 # change function between caps and escape - need it for vim.
 #setxkbmap -option caps:escape 
-
+#allow keys to be refreshing fast when holding 
+xset r rate 300 50
 #setxkbmap -option XF86AudioNext:F5
 
 
